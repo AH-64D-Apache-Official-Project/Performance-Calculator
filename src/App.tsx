08e131be18a7709feb06e341d857c5c18c89dedc
Matthew Smith <mattysmith22@googlemail.com>
2022-02-20 20:41:48 +0000

    Added export button to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { useState } from 'react';
 import './App.css';
 import {AircraftEditor, validateAircraft} from './AircraftEditor'
 import { calculatePerformance } from './perfCalcs';
@@ -9,6 +10,7 @@ import { EnvironmentEditor } from './EnvironmentEditor';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import { Exporter } from './exporter';
 
 const defaultAircraft : AircraftData = {
   fcrInstalled: true,
@@ -20,20 +22,29 @@ const defaultAircraft : AircraftData = {
   pylon4: {type: "none"}
 }
 
+interface AircraftMenuProps {
+  aircraft: AircraftData,
+  setAircraft: (ac:AircraftData) => void
+}
+function AircraftMenu ({aircraft}: AircraftMenuProps) {
+  return <Exporter aircraft={aircraft}/>
+}
+
 function App() {
-  const [aircraft,setAircraft] = React.useState(defaultAircraft)
-  const [environent,setEnvironent] = React.useState(defaultAircraftEnvironment)
+  const [aircraft,setAircraft] = useState(defaultAircraft)
+  const [environent,setEnvironent] = useState(defaultAircraftEnvironment)
 
   const perfData = calculatePerformance(aircraft, environent)
   return (
     <div className="App">
       <Container>
         <Row>
-          <Col md="7">
+          <Col lg="7">
+            <AircraftMenu aircraft={aircraft} setAircraft={setAircraft}/>
             <EnvironmentEditor environment={environent} setEnvironment={setEnvironent}/>
             <AircraftEditor aircraft={aircraft} setAircraft={(ac:AircraftData) => setAircraft(validateAircraft(ac))}/>
           </Col>
-          <Col>
+          <Col lg="5">
             <AircraftPerformanceView performance={perfData}/>
           </Col>
         </Row>
